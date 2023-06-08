fb9e8d52c5ebda8994ff6fc29550a0701d2a0cd7
Long Nguyen <nhlong143@gmail.com>
2023-06-08 09:35:45 +0700

    search icon update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { ReactComponent as Find } from "assets/header/search.svg";
+import find from "assets/header/search.svg";
 import { ReactComponent as Shop } from "assets/header/shop.svg";
 import { ReactComponent as Acc } from "assets/header/acc.svg";
 import { styled } from "styled-components";
@@ -10,6 +10,70 @@ const HeaderStyled = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+  .search-bar {
+    padding: 0px;
+    width: 50px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+  }
+  .search-box {
+    width: fit-content;
+    height: fit-content;
+    position: relative;
+  }
+  .input-search {
+    height: 50px;
+    width: 0px;
+    border-style: none;
+    padding: 10px;
+    font-size: 18px;
+    letter-spacing: 2px;
+    outline: none;
+    background-color: #f0f0f0;
+    border-radius: 25px;
+    transition: all 0.5s ease-in-out;
+    padding-right: 40px;
+    color: #0f0f0f;
+  }
+  .input-search::placeholder {
+    color: rgba(7, 7, 7, 0.5);
+    font-size: 18px;
+    letter-spacing: 2px;
+    font-weight: 100;
+  }
+  .btn-search {
+    border-style: none;
+    font-size: 20px;
+    font-weight: bold;
+    outline: none;
+    cursor: pointer;
+    border-radius: 50%;
+    position: absolute;
+    left: 0px;
+    color: #141414;
+    background-color: transparent;
+    pointer-events: painted;
+  }
+  .icon-size {
+    padding-top: 18px;
+    width: 40px;
+    height: 40px;
+  }
+  .btn-search:focus ~ .input-search {
+    width: 300px;
+    border-radius: 0px;
+    background-color: transparent;
+    padding-left: 50px;
+    transition: all 500ms cubic-bezier(0, 0.11, 0.35, 2);
+  }
+  .input-search:focus {
+    width: 300px;
+    padding-left: 50px;
+    border-radius: 0px;
+    background-color: transparent;
+    transition: all 500ms cubic-bezier(0, 0.11, 0.35, 2);
   }
   .brand {
     font-style: normal;
@@ -57,11 +121,49 @@ const HeaderStyled = styled.div`
     }
   }
 
+  @media (min-width: 1024px) and (max-width: 1919px) {
+    .btn-search:focus ~ .input-search {
+      width: 150px;
+    }
+    .input-search:focus {
+      width: 150px;
+    }
+  }
+  @media (min-width: 768px) and (max-width: 1023px) {
+    .content {
+      flex-wrap: wrap;
+    }
+    .search-bar {
+      width: 100%;
+    }
+    .btn-search:focus ~ .input-search {
+      width: 100%;
+    }
+    .input-search:focus {
+      width: 100%;
+    }
+  }
   @media (max-width: 767px) {
     .content {
       height: 120px;
       flex-direction: column;
       align-items: start;
+    }
+    .line {
+      margin-top: 35px;
+    }
+    .icon-size {
+      width: 35px;
+      height: 35px;
+    }
+    .search-bar {
+      width: 80%;
+    }
+    .btn-search:focus ~ .input-search {
+      width: 100%;
+    }
+    .input-search:focus {
+      width: 100%;
     }
   }
 `;
@@ -69,7 +171,18 @@ export const Header = () => {
   return (
     <HeaderStyled>
       <div className="content">
-        <Find />
+        <div className="search-bar">
+          <div className="search-box">
+            <button className="btn-search">
+              <img className="icon-size" src={find} alt="icon-search" />
+            </button>
+            <input
+              type="text"
+              class="input-search"
+              placeholder="Type to Search..."
+            />
+          </div>
+        </div>
         <span className="brand">Lisa Store</span>
         <div className="accs">
           <div className="shop">
